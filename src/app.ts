ae0a28158ee394dbc7a601c53465bf56dab2499f
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2021-07-02 17:39:18 +0200

    Simple GraphQL server init

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,28 @@
-import express from "express";
-import compression from "compression";  // compresses requests
-import lusca from "lusca";
-// import MongoStore from "connect-mongo";
-// import mongoose from "mongoose";
-// import bluebird from "bluebird";
-// import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
+import express from 'express';
+import compression from 'compression'; // compresses requests
+import lusca from 'lusca';
 
 // Controllers (route handlers)
-import * as apiController from "./controllers/api";
+import * as apiController from './controllers/api';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+
 
 
 // Create Express server
 const app = express();
 
-// Connect to MongoDB
-// const mongoUrl = MONGODB_URI;
-// mongoose.Promise = bluebird;
-
-// mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-//     () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-// ).catch(err => {
-//     console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
-//     // process.exit();
-// });
-
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(compression());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(session({
-//     resave: true,
-//     saveUninitialized: true,
-//     secret: SESSION_SECRET,
-//     store: new MongoStore({
-//         mongoUrl,
-//         mongoOptions: {
-//             autoReconnect: true
-//         }
-//     })
-// }));
-app.use(lusca.xframe("SAMEORIGIN"));
+
+app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
 
 /**
  * API examples routes.
  */
-app.get("/test", apiController.getTestCheck);
+app.get('/test', apiController.getTestCheck);
 
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
+import { ApolloServer } from "apollo-server-express";
 import errorHandler from "errorhandler";
 import app from "./app";
 
+import { typeDefs } from './graphql/types';
+import { resolvers } from './graphql/resolvers';
 
 /**
  * Error Handler. Provides full stack
@@ -8,7 +11,8 @@ import app from "./app";
 if (process.env.NODE_ENV === "development") {
     app.use(errorHandler());
 }
-
+export const apolloServer = new ApolloServer({ typeDefs, resolvers });
+apolloServer.applyMiddleware({app});
 
 /**
  * Start Express server.
@@ -17,8 +21,9 @@ const server = app.listen(app.get("port"), () => {
     console.log(
         "  App is running at http://localhost:%d in %s mode",
         app.get("port"),
-        app.get("env")
+        app.get("env"),
     );
+    console.log(`GraphQL is running at http://localhost:${app.get("port")}${apolloServer.graphqlPath}`)
     console.log("  Press CTRL-C to stop\n");
 });
 
